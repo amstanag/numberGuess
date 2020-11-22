a7aace1818070bc090e726be0febddd5ad499685
BR3AD <stonage3@gmail.com>
2020-11-21 20:54:19 -0500

    Initial Commit

---
+++ numberGuess/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  numberGuess
+//
+//  Created by Alex Stanage on 11/21/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
